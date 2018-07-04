0e7ffb315b158db1c0f49cbba85701b67c8f0866
JoeyRyanBridges <joeyryanbridges@gmail.com>
2018-07-04 09:40:27 -0700

    Working on porfolio, needing mobile responsive

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,11 +1,70 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
+import { Grid, Row, Col, Panel, Image, } from 'react-bootstrap'
 
 export default class Portfolio extends Component {
     render() {
         return (
-            <div>
-                <h1>Portfolio</h1>
-            </div>
+            <Grid>
+                <div className="container-fluid">
+                    <Row className="show-grid">
+                        <Col xs={6} md={4}>
+
+                            <Panel>
+                                <Panel.Heading>
+                                    <Panel.Title toggle><Image src="assets/goals.gif" className="gif" responsive /></Panel.Title>
+                                </Panel.Heading>
+                                <Panel.Body collapsible>Panel content 1</Panel.Body>
+                            </Panel>
+
+
+                        </Col>
+                        <Col xs={6} md={4}>
+                            <Panel>
+                                <Panel.Heading>
+                                    <Panel.Title toggle><Image src="assets/cook.gif" className="gif" responsive /></Panel.Title>
+                                </Panel.Heading>
+                                <Panel.Body collapsible>Panel content 1</Panel.Body>
+                            </Panel>
+                        </Col>
+                        <Col xs={6} md={4}>
+                            <Panel>
+                                <Panel.Heading>
+                                    <Panel.Title toggle><Image src="assets/simpsons.gif" className="gif" responsive /></Panel.Title>
+                                </Panel.Heading>
+                                <Panel.Body collapsible>Panel content 1</Panel.Body>
+                            </Panel>
+                        </Col>
+                    </Row>
+                    <Row className="show-grid">
+                        <Col xs={6} md={4}>
+                            <Panel>
+                                <Panel.Heading>
+                                    <Panel.Title toggle><Image src="assets/goals.gif" className="gif" responsive /></Panel.Title>
+                                </Panel.Heading>
+                                <Panel.Body collapsible>Panel content 1</Panel.Body>
+                            </Panel>
+                        </Col>
+                        <Col xs={6} md={4}>
+                            <Panel>
+                                <Panel.Heading>
+                                    <Panel.Title toggle><Image src="assets/goals.gif" className="gif" responsive /></Panel.Title>
+                                </Panel.Heading>
+                                <Panel.Body collapsible>Panel content 1</Panel.Body>
+                            </Panel>
+                        </Col>
+                        <Col xs={6} md={4}>
+                            <Panel>
+                                <Panel.Heading>
+                                    <Panel.Title toggle><Image src="assets/goals.gif" className="gif" responsive /></Panel.Title>
+                                </Panel.Heading>
+                                <Panel.Body collapsible>Panel content 1</Panel.Body>
+                            </Panel>
+                        </Col>
+                    </Row>
+
+
+                </div>
+            </Grid>
         )
     }
 }
