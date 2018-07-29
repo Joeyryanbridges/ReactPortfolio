263e16d31b38c2e7cfc843114bbfc1755eceafb6
JoeyRyanBridges <joeyryanbridges@gmail.com>
2018-07-28 23:34:59 -0700

    Fixing media queries since going live

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -30,27 +30,7 @@ export default class Contact extends React.Component {
             <div className="container">
                 <Form horizontal>
                     <FormGroup controlId="formHorizontalEmail">
-                        <Col componentClass={ControlLabel} sm={2}>
-                            Email
-    </Col>
-                        <Col smOffset={2} sm={10}>
-                            <FormControl type="email" placeholder="Email" />
-                        </Col>
-                    </FormGroup>
 
-                    <FormGroup controlId="formHorizontalPassword">
-                        <Col componentClass={ControlLabel} sm={2}>
-                            Password
-    </Col>
-                        <Col smOffset={2} sm={10}>
-                            <FormControl type="password" placeholder="Password" />
-                        </Col>
-                    </FormGroup>
-
-                    <FormGroup>
-                        <Col smOffset={2} sm={10}>
-                            <Button type="submit">Sign in</Button>
-                        </Col>
                     </FormGroup>
                 </Form>
             </div>
