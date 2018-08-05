42fec6b131f8424e7345e18b3bff84f1c5763521
JoeyRyanBridges <joeyryanbridges@gmail.com>
2018-08-04 23:33:13 -0700

    home in 375 done, beside switching icons

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -11,17 +11,18 @@ export default class Home extends Component {
                 </Jumbotron>
                 <br />
                 <center><h1>Welcome.</h1></center>
-                <div className="footer"><center className="iconsBaby">
-                    <a href="https://github.com/Joeyryanbridges">
-                        <Image src="giticon.png" className="githubIcon" />
-                    </a>
-                    <a href="https://www.linkedin.com/in/joeyrbridges/">
-                        <Image src="linkedinIcon.png" className="linkedinIcon" />
-                    </a>
-                    <a href="https://stackoverflow.com/users/9178799/joeyryanbridges">
-                        <Image src="SOFIcon.png" className="SOFIcon" />
-                    </a>
-                </center>
+                <div className="footer">
+                    <center className="iconsBaby">
+                        <a href="https://github.com/Joeyryanbridges">
+                            <Image src="giticon.png" className="githubIcon" />
+                        </a>
+                        <a href="https://www.linkedin.com/in/joeyrbridges/">
+                            <Image src="linkedinIcon.png" className="linkedinIcon" />
+                        </a>
+                        <a href="https://stackoverflow.com/users/9178799/joeyryanbridges">
+                            <Image src="SOFIcon.png" className="SOFIcon" />
+                        </a>
+                    </center>
                 </div>
             </Grid>
         )
